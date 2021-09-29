2a311079157ff73bb5383983464b7dbef4ca8757
DanilovG103 <danilov2706@gmail.com>
2021-09-29 10:36:03 +0400

    feat: add graphql

--- src/modules/character/index.tsx
+++ src/modules/character/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import styled from 'styled-components/native'
+
+const Container = styled.View`
+  padding: 15px;
+`
+
+const Title = styled.Text`
+  font-family: Montserrat-Bold;
+  font-size: 34px;
+`
 
 export const CharacterScreen = () => {
   return (
-    <View>
-      <Text>Characters</Text>
-    </View>
+    <Container>
+      <Title>Character</Title>
+    </Container>
   )
 }
